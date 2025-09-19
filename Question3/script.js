223c8619eb343e25d9689c1fa6dde7b2a009dffd
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-09-19 23:55:30 +0530

    Add files via upload

---
+++ Question3/script.js
@@ -0,0 +1,16 @@
+const input = document.getElementById("input");
+const button = document.getElementById("btn");
+const unorderedlist = document.getElementById("list");
+
+// Add event listener to the button
+button.addEventListener("click", ()=>{
+    const storeValue = input.value.trim();//store input values
+     // Only add if input is not empty also there should not be space adding
+     if(storeValue !== ""){
+     const listItem = document.createElement("li");// create element and store it in a variable
+     listItem.textContent = storeValue;
+     unorderedlist.appendChild(listItem);
+      // Clear the input box
+      input.value = "";
+     }
+});
