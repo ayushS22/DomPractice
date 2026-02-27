4b0e6c18f77c6d99600f2f98f150d9289a0dd28d
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-02-27 19:22:51 +0530

    Update script.js

--- DomPractice/Question1/script.js
+++ DomPractice/Question1/script.js
@@ -1,6 +1,7 @@
-const button = document.getElementById("btn");
+const button = document.getElementById("btn");//selecting button
 const paragraph = document.getElementById("para");
 
 button.addEventListener("click",()=>{
   paragraph.textContent = "Hello, DOM!";
+
 });
